d84e55c33bd0e3cab8f71877b0986f9ebc350b3a
Carlos Dávila <dev.carlos@hotmail.com>
2025-05-15 21:47:30 -0500

    add cd in ci cd workflow

--- src/0-split-dataset.py
+++ src/0-split-dataset.py
@@ -8,7 +8,7 @@ def split_data(df_data, perc_data_train):
     return df_data_train, df_data_test
 
 def process_split_data():
-    df_data = pd.read_csv("../data/in/application_data.csv")
+    df_data = pd.read_csv("data/in/application_data.csv")
     df_data_train, df_data_test = split_data(df_data, 0.7)
     df_data_train.to_csv("../data/out/application_data_train.csv", index=False)
     df_data_test.to_csv("../data/out/application_data_test.csv", index=False)
